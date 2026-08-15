db50bba35febee8e170965dababbf82b5366da3f
paraskcd1315 <dev@paraskcd.com>
2026-08-15 21:03:58 +0200

    KCDBAR-33: the start button opens Spotlight on Applications

---
+++ Sources/KcdBarBar/Domain/Windows/Utils/SpotlightMetrics.swift
@@ -0,0 +1,9 @@
+import CoreGraphics
+import Foundation
+
+/** The keys macOS 26 uses to open Spotlight and switch it to Applications. */
+package enum SpotlightMetrics {
+    package static let spaceKey: CGKeyCode = 49
+    package static let oneKey: CGKeyCode = 18
+    package static let applicationsDelay: TimeInterval = 0.22
+}
