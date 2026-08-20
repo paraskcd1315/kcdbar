704f941a31b3abaad7c27f7b3608d8a3c29ff556
paraskcd1315 <dev@paraskcd.com>
2026-08-20 03:31:40 +0200

    KCDBAR-85 step17: the Windows 11 mark takes the real logo's gap and corner

--- Sources/KcdBarDesignSystem/Ds/Atoms/KbWindowsMark/utils/KbWindowsMarkMetrics.swift
+++ Sources/KcdBarDesignSystem/Ds/Atoms/KbWindowsMark/utils/KbWindowsMarkMetrics.swift
@@ -1,9 +1,9 @@
 import CoreGraphics
 
 package enum KbWindowsMarkMetrics {
-    package static let paneRatio: CGFloat = 0.46
-    package static let gapRatio: CGFloat = 0.08
-    package static let paneRadiusRatio: CGFloat = 0.26
+    package static let paneRatio: CGFloat = 0.47
+    package static let gapRatio: CGFloat = 0.06
+    package static let paneRadiusRatio: CGFloat = 0.12
     package static let flagRiseRatio: CGFloat = 0.10
     package static let widthRatio: CGFloat = 1.0
 }
